bd49cdf16f7f90378cd56c442a96c802ea5b9806
Michael Meffie <mmeffie@sinenomine.net>
2020-07-14 19:33:04 -0400

    Rename example inventories
    Give the example inventories more meaningful names. Add an
    example inventory to install openafs from source (build).
    
    Use the inventory names to name the parametrized tests.
    
    Add a 'list' target to show the tests and add the PYTEST_OPT
    variable as a way to pass options like -k to run tests.

--- tests/playbooks/test_playbooks.py
+++ tests/playbooks/test_playbooks.py
@@ -11,46 +11,53 @@ env['ANSIBLE_NOCOLOR'] = 'yes'
 ansible = sh.Command('ansible')
 ansible_playbook = sh.Command('ansible-playbook')
 
+# Single host fixture.
 @pytest.fixture(params=['centos7', 'centos8', 'debian10'])
 def guest(request):
     template = 'base-%s' % request.param
     with Guest('afs01', template, update_resolver=True) as g:
-        for line in ansible('all', i='hosts/example01', m='wait_for_connection',
+        for line in ansible('all', i='hosts/single', m='wait_for_connection',
                             _cwd='../playbooks', _env=env, _iter=True):
             log.info('wait_for_connection: %s' % line.rstrip())
         yield(g)
 
-@pytest.fixture(params=[
-    ('hosts/example01', 1, 'centos7'),
-    ('hosts/example01', 1, 'centos8'),
-    ('hosts/example01', 1, 'debian10'),
-    ('hosts/example02', 9, 'centos7'),
+def invfn(val):
+    return '%s-%s' % (val[0].replace('hosts/', ''), val[2])
+
+# Test cell inventory fixture.
+@pytest.fixture(ids=invfn, params=[
+    ('hosts/single', 1, 'centos7'),
+    ('hosts/single', 1, 'centos8'),
+    ('hosts/single', 1, 'debian10'),
+    ('hosts/multi', 9, 'centos7'),
+    ('hosts/build', 3, 'centos7'),
 ])
 def inventory(request):
-    inv, num, os_ = request.param
+    hosts, num, os_ = request.param
     guests = []
     for i in range(num):
         g = Guest('afs%02d' % (i + 1), 'base-%s' % os_, update_resolver=True)
         guests.append(g)
     for g in guests:
         g.create()
-    for line in ansible('all', i=inv, m='wait_for_connection',
+    for line in ansible('all', i=hosts, m='wait_for_connection',
                          _cwd='../playbooks', _env=env, _iter=True):
         log.info('wait_for_connection: %s' % line.rstrip())
-    yield(inv)
+    yield(hosts)
     for g in guests:
         g.destroy()
 
-@pytest.mark.parametrize('playbook', ['build01.yaml', 'build02.yaml', 'build03.yaml'])
-def test_build(guest, playbook):
-    log.info('Running playbook %s on guest %s' % (playbook, guest.name))
-    for line in ansible_playbook(playbook, i='hosts/example01',
-                                 _cwd='../playbooks', _env=env, _iter=True):
+# Helper to run a playbook.
+def run_playbook(playbook, inventory):
+    log.info('Running playbook %s' % playbook)
+    for line in ansible_playbook(playbook, i=inventory, _cwd='../playbooks', _env=env, _iter=True):
         log.info('ansible: %s', line.strip())
 
+# Test build-only playbooks.
+@pytest.mark.parametrize('playbook', ['build01.yaml', 'build02.yaml', 'build03.yaml'])
+def test_build(guest, playbook):
+    run_playbook(playbook, 'hosts/single')
+
+# Create test cells and run robotest.
 def test_testcell(inventory):
-    playbook = 'testcell.yaml'
-    log.info('Running %s on inventory %s' % (playbook, inventory))
-    for line in ansible_playbook(playbook, i='hosts/example01',
-                                 _cwd='../playbooks', _env=env, _iter=True):
-        log.info('ansible: %s', line.strip())
+    run_playbook('testcell.yaml', inventory)
