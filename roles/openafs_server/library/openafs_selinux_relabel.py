2e68b2df326a58f4eae6512b577d6e9f18283ded
Michael Meffie <mmeffie@sinenomine.net>
2021-01-05 13:28:38 -0500

    Add openafs_selinux_relabel module
    Move the restorecon invocations to a module to make it idempotent and to
    simplify the code to check the partitions.  We cheat by just remembering
    which directories were relabelled by saving them in the openafs.fact
    file on the remote node.

---
+++ roles/openafs_server/library/openafs_selinux_relabel.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# Copyright (c) 2021, Sine Nomine Associates
+# BSD 2-Clause License
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1.',
+    'status': ['preview'],
+    'supported_by': 'community',
+}
+
+DOCUMENTATION = r"""
+---
+module: openafs_selinux_relabel
+short_description: Relabel selinux context for server files.
+description:
+  - Relabel the server directories after the files have been installed
+    and the configuration files updated.
+  - Relabel the partition directories and the AlwaysAttach file, when present.
+  - Safe the list of directories relabelled in the openafs local facts file
+    to avoid running restorecon on subsequent plays.
+"""
+
+EXAMPLES = r"""
+- name: Relabel
+  become: yes
+  openafs_selinux_relabel:
+"""
+
+RETURN = r"""
+"""
+
+import glob
+import json
+import logging
+import os
+import pprint
+from ansible.module_utils.basic import AnsibleModule
+
+log = logging.getLogger(__name__)
+
+# Note: The bosserver creates the /usr/vice/etc directory if it does not
+# exist in order to create a symlink to the server configuration. Be sure
+# to set the selinux context on /usr/vice before the bosserver starts.
+top_dirs = ['/usr/afs', '/usr/vice']
+
+def main():
+    results = dict(
+        changed=False,
+    )
+    module = AnsibleModule(
+            argument_spec=dict(
+            ),
+            supports_check_mode=False,
+    )
+
+    logfile = '/var/log/ansible-openafs/openafs_selinux_relabel.log'
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=logfile,
+        format='%(asctime)s %(levelname)s %(message)s',
+    )
+
+    def restorecon(*args):
+        restorecon = module.get_bin_path('restorecon', required=True)
+        cmdargs = [restorecon] + list(args)
+        cmdline = ' '.join(cmdargs)
+        log.info("Running: %s", cmdline)
+        rc, out, err = module.run_command(cmdargs)
+        if rc != 0:
+            log.error("Command failed: %s, rc=%d, err=%s", cmdline, rc, err)
+            module.fail_json(msg="Command failed", cmd=cmdline, out=out, err=err)
+
+    log.info('Starting openafs_selinux_relabel')
+    log.debug('Parameters: %s' % pprint.pformat(module.params))
+
+    factsfile = '/etc/ansible/facts.d/openafs.fact'
+    try:
+        with open(factsfile) as fp:
+            facts = json.load(fp)
+    except:
+        facts = {}
+
+    changed = []
+    relabelled = facts.get('relabelled', [])
+
+    for path in top_dirs:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        if path not in relabelled:
+            restorecon('-i', '-r', path)
+            changed.append(path)
+
+    for path in glob.glob('/vicep*'):
+        if path not in relabelled:
+            restorecon('-i', path)
+            changed.append(path)
+
+    for path in glob.glob('/vicep*/AlwaysAttach'):
+        if path not in relabelled:
+            restorecon(path)
+            changed.append(path)
+
+    if changed:
+        facts['relabelled'] = sorted(set(relabelled) | set(changed))
+        if not os.path.exists(os.path.dirname(factsfile)):
+            os.makedirs(os.path.dirname(factsfile))
+        with open(factsfile, 'w') as fp:
+            json.dump(facts, fp, indent=2)
+        results['changed'] = True
+
+    log.debug('Results: %s' % pprint.pformat(results))
+    log.info('Exiting openafs_selinux_relabel')
+    module.exit_json(**results)
+
+if __name__ == '__main__':
+    main()
