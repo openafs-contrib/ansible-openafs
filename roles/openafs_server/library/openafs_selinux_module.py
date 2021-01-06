ea369ace5548d233e3f4be8ac7725705b229ee96
Michael Meffie <mmeffie@sinenomine.net>
2021-01-06 13:30:40 -0500

    Add openafs_selinux_module
    Move commands to generate the selinux module to a custom idempotent
    ansible module. This also gets rid of the shell pipeline in the yaml.
    Get rid if the "module version" variable, and just check the version
    number in the type enforcement (te) file directly.

---
+++ roles/openafs_server/library/openafs_selinux_module.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+# Copyright (c) 2021, Sine Nomine Associates
+# BSD 2-Clause License
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1.',
+    'status': ['preview'],
+    'supported_by': 'community',
+}
+
+DOCUMENTATION = r"""
+---
+module: openafs_selinux_module
+short_description: Create and install an selinux module from input files
+description: Build the selinux module from the given input files.
+options:
+  state:
+    description: c(present) is currently the only supported state.
+  name:
+    description: name of the selinux module
+    default: openafs
+  path:
+    description:
+      - Path to the Type Enforcement (te) and File Context (fc) input files and
+        the destination path of the output pp and mod files.
+    default: /var/lib/ansible-openafs/selinux
+"""
+
+EXAMPLES = r"""
+- name: Copy the SELinux module definitions for openafs server
+  become: yes
+  template:
+    dest: "/var/lib/ansible-openafs/selinux/{{ item }}"
+    src: "{{ role_path }}/templates/{{ item }}.j2"
+  with_items:
+    - openafs.te
+    - openafs.fc
+
+- name: Build SELinux module for openafs server
+  become: yes
+  openafs_selinux_module:
+    name: openafs
+    path: /var/lib/ansible-openafs/selinux
+"""
+
+RETURN = r"""
+module": "/var/lib/ansible-openafs/selinux/openafs.mod",
+version": null
+"""
+
+import logging
+import os
+import pprint
+import re
+from ansible.module_utils.basic import AnsibleModule
+
+log = logging.getLogger(__name__)
+
+def main():
+    results = dict(
+        changed=False,
+    )
+    module = AnsibleModule(
+            argument_spec=dict(
+                state=dict(type='str', choices=['present'], default='present'),
+                name=dict(type='str', default='openafs'),
+                path=dict(type='path', default='/var/lib/ansible-openafs'),
+            ),
+            supports_check_mode=False,
+    )
+
+    name = module.params['name']
+    path = module.params['path']
+
+    logfile = '/var/log/ansible-openafs/openafs_selinux_module.log'
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=logfile,
+        format='%(asctime)s %(levelname)s %(message)s',
+    )
+
+    def die(msg):
+        log.error(msg)
+        module.fail_json(msg=msg)
+
+    def run_command(cmd, *args):
+        cmdargs = [module.get_bin_path(cmd, required=True)] + list(args)
+        cmdline = ' '.join(cmdargs)
+        log.info("Running: %s", cmdline)
+        rc, out, err = module.run_command(cmdargs)
+        if rc != 0:
+            die("Command failed: %s, rc=%d, err=%s" % (cmdline, rc, err))
+        return out
+
+    def checkmodule(*args):
+        return run_command('checkmodule', *args)
+
+    def semodule_package(*args):
+        return run_command('semodule_package', *args)
+
+    def semodule(*args):
+        return run_command('semodule', *args)
+
+    log.info('Starting openafs_selinux_module')
+    log.debug('Parameters: %s' % pprint.pformat(module.params))
+
+    if not name:
+        die("Module name is required.")
+
+    te = os.path.join(path, '%s.te' % name)
+    fc = os.path.join(path, '%s.fc' % name)
+    for f in (te, fc):
+        if not os.path.exists(f):
+            die("Input file '%s' not found." % f)
+
+    current_version = None
+    target_version = None
+
+    out = semodule('-lstandard')
+    for line in out.splitlines():
+        m = re.match(r'\s*(\S+)\s+(\S+)\s*', line)
+        if m and m.group(1) == name:
+            current_version = m.group(2)
+            break
+
+    if not current_version:
+        log.info("%s module is not installed.", name)
+    else:
+        log.info("%s module version is '%s'.", name, current_version)
+        try:
+            with open(te) as f:
+                for line in f.readlines():
+                    m = re.match(r'\s*module\s+(\S+)\s+(\S+)\s*;', line)
+                    if m and m.group(1) == name:
+                        target_version = m.group(2)
+                        break
+        except Exception as e:
+            die("Failed to read te file: %s" % str(e))
+        if not target_version:
+            die("SELinux module version number not found in file '%s'." % te)
+        log.info("%s module target version is '%s'.", name, target_version)
+
+    if not current_version or (current_version != target_version):
+        mod = os.path.join(path, '%s.mod' % name)
+        pp = os.path.join(path, '%s.pp' % name)
+        checkmodule('-M', '-m', '-o', mod, te)
+        semodule_package('-o', pp, '-m', mod, '-f', fc)
+        semodule('-i', pp)
+        results['changed'] = True
+
+    log.debug('Results: %s' % pprint.pformat(results))
+    log.info('Exiting openafs_selinux_module')
+    module.exit_json(**results)
+
+if __name__ == '__main__':
+    main()
